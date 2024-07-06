e266f17454a4ff154a4b0c0287da2c7fcc607b91
codermaccoil <codermaccoil@gmail.com>
2024-07-06 17:35:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-dvh bg-ggLight">
+        <div className="container mx-auto">
+          <div className="flex justify-center items-center h-screen">
+            <div className="text-white text-4xl font-bold">
+              <img src="./logo.svg" alt="The logo of goGaeilge" className="w-48" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      ggLight: "#D0D9D4",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
